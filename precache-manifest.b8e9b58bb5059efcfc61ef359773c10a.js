9c4eb71b5ddb17e25af273936a63f108c1d49db5
Der Meyer <simon.der.meyer@gmail.com>
2020-10-25 21:09:04 +0100

    Glen Coden auto commit at 2020-10-25T20:09:04.884Z

---
+++ precache-manifest.b8e9b58bb5059efcfc61ef359773c10a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae37bcad2416c0eb78c9199f33e03c70",
+    "url": "./index.html"
+  },
+  {
+    "revision": "38c6a6591f820e06bc4d",
+    "url": "./static/css/main.f940d420.chunk.css"
+  },
+  {
+    "revision": "8a45f837bc10bf5c57d5",
+    "url": "./static/js/2.4a712835.chunk.js"
+  },
+  {
+    "revision": "11da4d0289ce192632a3fc6590a86fde",
+    "url": "./static/js/2.4a712835.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38c6a6591f820e06bc4d",
+    "url": "./static/js/main.3950bcca.chunk.js"
+  },
+  {
+    "revision": "972b4be6f8440bad6b16",
+    "url": "./static/js/runtime-main.4558fc93.js"
+  },
+  {
+    "revision": "78a9da9b9cfd54cee52df8f1adfc63e3",
+    "url": "./static/media/coffee.78a9da9b.svg"
+  },
+  {
+    "revision": "1074bb9f38f7e87979c5778a6a976b2e",
+    "url": "./static/media/desk.1074bb9f.svg"
+  },
+  {
+    "revision": "044364e49ec717736a9aa84931568f31",
+    "url": "./static/media/garten.044364e4.svg"
+  },
+  {
+    "revision": "4df8bd8144f443ce308f768902820a70",
+    "url": "./static/media/instagram.4df8bd81.svg"
+  },
+  {
+    "revision": "db7c83667cd51426a448757474934c16",
+    "url": "./static/media/kitchen.db7c8366.svg"
+  },
+  {
+    "revision": "573662c8d874400163c4257b5c210832",
+    "url": "./static/media/logo.573662c8.svg"
+  },
+  {
+    "revision": "61f2b1f4dcd2d10f7e060914c03693bf",
+    "url": "./static/media/meeting.61f2b1f4.svg"
+  },
+  {
+    "revision": "2f32b0d5fc917391b9a08f0b4463b715",
+    "url": "./static/media/oekostrom.2f32b0d5.svg"
+  },
+  {
+    "revision": "5df3b6ed0a9554c7d13ebbd4d6e272f2",
+    "url": "./static/media/preis.5df3b6ed.svg"
+  },
+  {
+    "revision": "4915fd88502359b707b964cfe302c609",
+    "url": "./static/media/reinigung.4915fd88.svg"
+  },
+  {
+    "revision": "9aea7df75fec3637d4209abedbd486c7",
+    "url": "./static/media/rollcontainer.9aea7df7.svg"
+  },
+  {
+    "revision": "87942adf748d3ffa70429e88bf717649",
+    "url": "./static/media/telefon.87942adf.svg"
+  },
+  {
+    "revision": "aaba65d277c0deeaafc6e2a4b03bb40a",
+    "url": "./static/media/verkehr.aaba65d2.svg"
+  },
+  {
+    "revision": "b86be8626d7a3827454a448436439534",
+    "url": "./static/media/wifiicon.b86be862.svg"
+  }
+]);
